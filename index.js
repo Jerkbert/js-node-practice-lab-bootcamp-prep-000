cec53d467cba2131e000ec4ad561ad4e59031875
Rob Ballantyne <raballantyne@gmail.com>
2019-05-17 17:46:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Rob"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name + " the Bob")
 
 console.log("Height:")
-console.log(height + " CM")
+console.log(height + 5 + " CM")
